3013af3d6302cff09ac2fce751a7c15637110aa5
lealbal <lealbernaz.lme@gmail.com>
2021-12-27 07:52:16 -0300

    Versão 0 Início

---
+++ Dash_Inicial/app.R
@@ -0,0 +1,61 @@
+#
+# This is a Shiny web application. You can run the application by clicking
+# the 'Run App' button above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    http://shiny.rstudio.com/
+#
+
+library(shiny)
+
+# Define UI for application that draws a histogram
+ui <- fluidPage(
+
+    # App title ----
+    titlePanel("Hello World!"),
+    
+    # Sidebar layout with input and output definitions ----
+    sidebarLayout(
+        
+        # Sidebar panel for inputs ----
+        sidebarPanel(
+            
+            # Input: Slider for the number of bins ----
+            sliderInput(inputId = "bins",
+                        label = "Number of bins:",
+                        min = 5,
+                        max = 50,
+                        value = 30)
+            
+        ),
+        
+        # Main panel for displaying outputs ----
+        mainPanel(
+            
+            # Output: Histogram ----
+            plotOutput(outputId = "distPlot")
+            
+        )
+    )
+)
+
+# Define server logic required to draw a histogram
+server <- function(input, output) {
+
+    output$distPlot <- renderPlot({
+        
+        x    <- faithful$waiting
+        bins <- seq(min(x), max(x), length.out = input$bins + 1)
+        
+        hist(x, breaks = bins, col = "#75AADB", border = "orange",
+             xlab = "Waiting time to next eruption (in mins)",
+             main = "Histogram of waiting times")
+        
+    })
+}
+
+# Run the application 
+shinyApp(ui = ui, server = server)
+
+
